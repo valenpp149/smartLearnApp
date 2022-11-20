cfe485b3abc7b926daa06337d1ad38989023ca7c
valenpp149 <112736805+valenpp149@users.noreply.github.com>
2022-11-20 18:45:37 -0500

    sprint2

--- app/src/main/java/com/example/smartlearnapp/view/ui/activities/LoginActivity.kt
+++ app/src/main/java/com/example/smartlearnapp/view/ui/activities/LoginActivity.kt
@@ -1,15 +1,49 @@
 package com.example.smartlearnapp.view.ui.activities
 
 import android.app.Activity
+import android.content.Context
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
+import android.widget.Toast
 import com.example.smartlearnapp.R
+import com.example.smartlearnapp.databinding.ActivityLoginBinding
 
 
 class LoginActivity : Activity() {
+    lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.ingreso.setOnClickListener{validar()}
+        binding.registro.setOnClickListener{irARegistro()}
+    }
+    fun validar(){
+        val correo: String = binding.email.text.toString()
+        val clave: String = binding.password.text.toString()
+
+        var pref = getSharedPreferences(correo, Context.MODE_PRIVATE)
+        var correo2 = pref.getString("email", "")
+        var clave2 = pref.getString("contraseña", "")
+        if(correo.isEmpty()){
+            binding.email.setHint("Ingrese su correo")
+            binding.email.setHintTextColor(Color.RED)
+        }else if(clave.isEmpty()){
+            binding.password.setHint("Ingrese su contraseña")
+            binding.password.setHintTextColor(Color.RED)
+        }else if(correo==correo2){
+            if(clave==clave2){
+                Toast.makeText(this, "Bienvenido", Toast.LENGTH_LONG).show()
+            }else{
+                Toast.makeText(this, "Contraseña incorrecta", Toast.LENGTH_LONG).show()
+            }
+        }else{
+            Toast.makeText(this, "Usuario no registrado", Toast.LENGTH_LONG).show()
+        }
     }
 
+    fun irARegistro(){
+        startActivity(Intent(this, registrarActivity::class.java))
+    }
 }
---
+++ app/src/main/java/com/example/smartlearnapp/view/ui/activities/registrarActivity.kt
@@ -0,0 +1,41 @@
+package com.example.smartlearnapp.view.ui.activities
+
+import android.app.Activity
+import android.content.Context
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.smartlearnapp.databinding.ActivityLoginBinding
+import com.example.smartlearnapp.databinding.ActivityRegistrarBinding
+
+class registrarActivity : Activity(){
+    lateinit var binding: ActivityRegistrarBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityRegistrarBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.registrar.setOnClickListener{guardarUsuario()}
+
+    }
+
+    fun guardarUsuario(){
+        val nombre:String = binding.nombree.text.toString()
+        val cedula:String = binding.identificacion.text.toString()
+        val correo:String = binding.correoo.text.toString()
+        val direccion:String = binding.direccion.text.toString()
+        val telefono:String = binding.telefono.text.toString()
+        val clave:String = binding.clavee.text.toString()
+
+        var pref =getSharedPreferences(correo, Context.MODE_PRIVATE)
+        var editar= pref.edit()
+        editar.putString("email", correo)
+        editar.putString("nombre", nombre)
+        editar.putString("identificacion", cedula)
+        editar.putString("direccion", direccion)
+        editar.putString("telefono", telefono)
+        editar.putString("contraseña", clave)
+
+        editar.commit()
+        Toast.makeText(this, "Usuario registrado exitosamente", Toast.LENGTH_LONG).show()
+    }
+}
